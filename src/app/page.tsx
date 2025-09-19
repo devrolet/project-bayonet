ca546d17290fc76e15b1ee8602d47ff010dd9745
Chet Hill <chetdhill@gmail.com>
2025-09-19 06:54:37 -0500

    Welcome to Project Bayonet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
+  
   return (
    <div>
-    <h1>Hello Home Page</h1>
+    <h1>Welcome to Project Bayonet</h1>
    </div>
   );
 }
