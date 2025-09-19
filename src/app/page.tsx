d131ef015986b85b6da560f5307721ecf4219876
Chet Hill <chetdhill@gmail.com>
2025-09-19 18:45:29 -0500

    Like, Comment, & Delete Post functionality, BUG: Delete post icon doesn't show as expected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,30 @@
 
 import CreatePost from "@/components/CreatePost";
-// import PostCard from "@/components/PostCard";
+import PostCard from "@/components/PostCard";
 import WhoToFollow from "@/components/WhoToFollow";
 import { currentUser } from "@clerk/nextjs/server";
+import { getPosts } from "@/actions/post.action";
+import { getDbUserId } from "@/actions/user.action";
 
 export default async function Home() {
   const user = await currentUser();
+  const posts = await getPosts();
+  const dbUserId = getDbUserId();
+
+  // console.log({ posts });
+  
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
       <div className="lg:col-span-6">
+
         {user ? <CreatePost /> : null}
+
+        <div className="space-y-6">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+          ))}
+        </div>
       </div>
 
       <div className="hidden lg:block lg:col-span-4 sticky top-20">
