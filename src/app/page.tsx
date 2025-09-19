d0c72d2b700372b3b252d1f7dcf8d262f2f7a1c5
Chet Hill <chetdhill@gmail.com>
2025-09-19 14:52:08 -0500

    Create WhoToFollow component, confirm it pulls random users from DB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 
 import CreatePost from "@/components/CreatePost";
-import PostCard from "@/components/PostCard";
+// import PostCard from "@/components/PostCard";
 import WhoToFollow from "@/components/WhoToFollow";
 import { currentUser } from "@clerk/nextjs/server";
 
