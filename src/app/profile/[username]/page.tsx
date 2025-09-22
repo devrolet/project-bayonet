a21bd6c9a7ab5721311233ae5874b6b4eafd79fa
Chet Hill <chetdhill@gmail.com>
2025-09-22 10:43:31 -0500

    Complete ProilePage and ProfilePageClient UI and functionality

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,11 +1,38 @@
-async function ProfilePage({params}: {params: {username: string } }) {
-  console.log(params);
+import { getProfileByUsername, getUserLikedPosts, getUserPosts, isFollowing } from "@/actions/profile.action";
+import { notFound } from "next/navigation";
+import ProfilePageClient from "./ProfilePageClient";
+
+export async function generateMetadata({ params }: { params: {username: string } }) {
+  const user = await getProfileByUsername(params.username);
+  if(!user) return;
+
+  return {
+    title: `${user.name ?? user.username}`,
+    description: user.bio || `Check out ${user.username}'s profile.`,
+  }
+}
+
+async function ProfilePageServer({params}: {params: {username: string } }) {
+  const user = await getProfileByUsername(params.username);
+
+  if(!user) notFound();
+
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ]);
 
   await new Promise((resolve) => setTimeout(resolve, 3000));
 
   return (
-    <div>Profile Page</div>
+    <ProfilePageClient
+      user={user}
+      posts={posts}
+      likedPosts={likedPosts}
+      isFollowing={isCurrentUserFollowing}
+    />
   )
 }
 
-export default ProfilePage
+export default ProfilePageServer
